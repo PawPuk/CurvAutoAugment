bea7951adb0d658a45cad394fcd50bc66d4ed2dc
PawPuk <ppukowski1@sheffield.ac.uk>
2024-02-07 17:08:13 +0000

    *Training on stragglers results in better generalisation*
    First I identified stragglers of each class manifold using the method from (https://www.nature.com/articles/s42256-023-00772-9). A total of 6599 stragglers and 63401 non-stragglers were found within the MNIST dataset. Next I trained two models with the same architecture. First one was trained on stragglers and evaluated on non-stragglers, and the other was trained on non-stragglers and evaluated on non-stragglers. I repeated the same experiment 5 times. The models achieved the train accuracy of 99.67% ± 0.1 when training on stragglers, and test accuracy of 56.23% ± 0.8. On the other hand, when trained on non-stragglers it achieved train accuracy of 99.98% ± 0.005 and test accuracy of 33.58% ± 1.1.

--- Identifying_stragglers/verifying_importance_of_stragglers.py
+++ Identifying_stragglers/verifying_importance_of_stragglers.py
@@ -1,15 +1,12 @@
 import copy
-import os
 
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
 from neural_networks import SimpleNN
-from utils import (calculate_percentiles, calculate_statistics, load_data, load_statistics, plot_radii, plot_statistics,
-                   test, train_model, train_stop_at_inversion, transform_datasets_to_dataloaders)
+from utils import (load_data, test, train_model, train_stop_at_inversion, transform_datasets_to_dataloaders)
 
 # Load the dataset. We copy the batch size from the "Inversion dynamics of class manifolds in deep learning ..." paper
 train_dataset, test_dataset, full_dataset = load_data()
@@ -39,18 +36,13 @@ for data, target in full_loader:
             # Concatenate the non-straggler data and targets
             non_straggler_data = torch.cat((non_straggler_data, data[current_non_stragglers]), dim=0)
             non_straggler_target = torch.cat((non_straggler_target, target[current_non_stragglers]), dim=0)
-print(f'The model achieved the test accuracy of {round(test(model, train_loader), 4)}%, and '
+print(f'The model achieved the train accuracy of {round(test(model, train_loader), 4)}%, and '
       f'{round(test(model, test_loader), 4)}% on the test set')
-print(len(straggler_data), len(straggler_target))
-print(len(non_straggler_data), len(non_straggler_target))
-# Create TensorDataset for stragglers
+print(f'A total of {len(straggler_data)} stragglers and {len(non_straggler_data)} non-stragglers were found.')
+# Create TensorDataset and DataLoader for stragglers and non-stragglers
 straggler_dataset = TensorDataset(straggler_data, straggler_target)
-# Create DataLoader for stragglers
-straggler_loader = DataLoader(straggler_dataset, batch_size=64, shuffle=True)
-
-# Create TensorDataset for non-stragglers
 non_straggler_dataset = TensorDataset(non_straggler_data, non_straggler_target)
-# Create DataLoader for non-stragglers
+straggler_loader = DataLoader(straggler_dataset, batch_size=64, shuffle=True)
 non_straggler_loader = DataLoader(non_straggler_dataset, batch_size=64, shuffle=True)
 for _ in range(5):
     model = SimpleNN(28*28, 2, 40, 1)
@@ -58,8 +50,10 @@ for _ in range(5):
     optimizer = optim.SGD(model.parameters(), lr=0.1)
     optimizer1 = optim.SGD(model1.parameters(), lr=0.1)
     _, _ = train_model(model, straggler_loader, optimizer, criterion, num_epochs, False)
-    print(f'The model achieved the train accuracy of {round(test(model, straggler_loader), 4)}% when training on '
-          f'stragglers, and {round(test(model, full_loader), 4)}% on the whole dataset')
+    print(f'The model achieved the train accuracy of {round(test(model, straggler_loader, False), 4)}% when training on'
+          f' stragglers, and test accuracy of {round(test(model, non_straggler_loader, False), 4)}%. This is equivalent'
+          f'to the accuracy of {round(test(model, full_loader, False), 4)}% on the whole dataset.')
     _, _ = train_model(model1, non_straggler_loader, optimizer1, criterion, num_epochs, False)
-    print(f'The model achieved the train accuracy of {round(test(model1, non_straggler_loader), 4)}% when training on '
-          f'non-stragglers, and {round(test(model1, full_loader), 4)}% on the whole dataset')
+    print(f'The model achieved the train accuracy of {round(test(model1, non_straggler_loader, False), 4)}% when '
+          f'training on non-stragglers, and test accuracy of {round(test(model1, straggler_loader, False), 4)}%. This '
+          f'is equivalent to the accuracy of {round(test(model1, full_loader, False), 4)}% on the whole dataset.')
