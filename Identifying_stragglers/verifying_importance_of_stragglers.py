6754503167b3e1224acfe2f6d78c5c6ba4708b84
PawPuk <ppukowski1@sheffield.ac.uk>
2024-02-08 08:39:45 +0000

    Made the experiments more robust and added visiaulisation of the results. The results thmselves are just as I expected.

--- Identifying_stragglers/verifying_importance_of_stragglers.py
+++ Identifying_stragglers/verifying_importance_of_stragglers.py
@@ -1,12 +1,13 @@
 import copy
 
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
 
 from neural_networks import SimpleNN
-from utils import (load_data, test, train_model, train_stop_at_inversion, transform_datasets_to_dataloaders)
+from utils import (load_data, straggler_ratio_vs_generalisation, train_stop_at_inversion,
+                   transform_datasets_to_dataloaders)
 
 # Load the dataset. We copy the batch size from the "Inversion dynamics of class manifolds in deep learning ..." paper
 train_dataset, test_dataset, full_dataset = load_data()
@@ -36,24 +37,15 @@ for data, target in full_loader:
             # Concatenate the non-straggler data and targets
             non_straggler_data = torch.cat((non_straggler_data, data[current_non_stragglers]), dim=0)
             non_straggler_target = torch.cat((non_straggler_target, target[current_non_stragglers]), dim=0)
-print(f'The model achieved the train accuracy of {round(test(model, train_loader), 4)}%, and '
-      f'{round(test(model, test_loader), 4)}% on the test set')
 print(f'A total of {len(straggler_data)} stragglers and {len(non_straggler_data)} non-stragglers were found.')
-# Create TensorDataset and DataLoader for stragglers and non-stragglers
-straggler_dataset = TensorDataset(straggler_data, straggler_target)
-non_straggler_dataset = TensorDataset(non_straggler_data, non_straggler_target)
-straggler_loader = DataLoader(straggler_dataset, batch_size=64, shuffle=True)
-non_straggler_loader = DataLoader(non_straggler_dataset, batch_size=64, shuffle=True)
-for _ in range(5):
-    model = SimpleNN(28*28, 2, 40, 1)
-    model1 = copy.deepcopy(model)
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-    optimizer1 = optim.SGD(model1.parameters(), lr=0.1)
-    _, _ = train_model(model, straggler_loader, optimizer, criterion, num_epochs, False)
-    print(f'The model achieved the train accuracy of {round(test(model, straggler_loader, False), 4)}% when training on'
-          f' stragglers, and test accuracy of {round(test(model, non_straggler_loader, False), 4)}%. This is equivalent'
-          f'to the accuracy of {round(test(model, full_loader, False), 4)}% on the whole dataset.')
-    _, _ = train_model(model1, non_straggler_loader, optimizer1, criterion, num_epochs, False)
-    print(f'The model achieved the train accuracy of {round(test(model1, non_straggler_loader, False), 4)}% when '
-          f'training on non-stragglers, and test accuracy of {round(test(model1, straggler_loader, False), 4)}%. This '
-          f'is equivalent to the accuracy of {round(test(model1, full_loader, False), 4)}% on the whole dataset.')
+straggler_ratios, avg_accuracies, std_accuracies = straggler_ratio_vs_generalisation(
+    straggler_data, straggler_target, non_straggler_data, non_straggler_target)
+
+plt.errorbar(straggler_ratios, avg_accuracies, yerr=std_accuracies, marker='o', capsize=5)
+plt.xlabel('Ratio of Stragglers in Train Set')
+plt.ylabel('Accuracy on Test Set (%)')
+plt.title('Test Set Accuracy vs. Straggler Ratio')
+plt.grid(True)
+plt.savefig('Figures/generalisation_vs_straggler_ratio.png')
+plt.savefig('Figures/generalisation_vs_straggler_ratio.pdf')
+plt.show()
